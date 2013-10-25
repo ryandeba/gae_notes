7bf317a93ab9a4a09baf2a24343ed944cab355fd
ryandeba <ryandeba@gmail.com>
2013-10-24 23:32:54 -0500

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+import webapp2
+import os
+from google.appengine.ext.webapp import template
+
+class index(webapp2.RequestHandler):
+    def get(self):
+        path = os.path.join(os.path.dirname(__file__), 'views/index.html')
+        self.response.write(template.render(path, {}))
+
+application = webapp2.WSGIApplication([
+    ('/', index),
+], debug=True)
